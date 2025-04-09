b6e3e5d33eb487905d806f966c276ef876d1a75b
minji0000 <bluurmi@gmail.com>
2025-04-09 23:50:46 +0900

    build 성공

--- letterFromThePast/src/main/java/org/letter/letterfromthepast/controller/LetterController.java
+++ letterFromThePast/src/main/java/org/letter/letterfromthepast/controller/LetterController.java
@@ -1,16 +1,15 @@
 package org.letter.letterfromthepast.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //RestController의 주용도는 Json 형태로 객체 데이터를 반환하는 것
-@RestController
+//Controller는 return 값을 뷰 이름으로 인식한다.
+@Controller
 public class LetterController{
     @GetMapping("/letter")
     public String letter(){
-        return "/letter/letterMainView";
+        return "letter/letterMainView";
     }
 
     //git..
--- letterFromThePast/src/test/java/org/letter/letterfromthepast/LetterFromThePastApplicationTests.java
+++ letterFromThePast/src/test/java/org/letter/letterfromthepast/LetterFromThePastApplicationTests.java
@@ -1,13 +1,14 @@
 package org.letter.letterfromthepast;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LetterFromThePastApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("JUnit 5 테스트 실행");
     }
 
 }
